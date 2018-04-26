9a5683566a3fda898a0ccd4dd1a85adddf7e2913
vangx033 <seeho.vang@hotmail.com>
2018-04-25 18:54:21 -0700

    AccessingMinecraftServer.md

--- AccessingMinecraftServer.sh
+++ AccessingMinecraftServer.sh
@@ -1,1 +1,10 @@
-How to connect to the Minecraft server
+Here are the steps on how to connect to the Minecraft server, please skip to step #5
+---
+1) Go to https://minecraft.net/en-us and click 'Buy Minecraft' to purchase the game.
+2) Enter your address email, password and DOB to create an account
+3) Choose your operation system (Windows or MAC) and install the application
+4) Lauch the application and you should see more options such as Single Player, Multiplayer and ect.
+4) Click one that you would like to play and click "Add Server". Enter the name and the IP address of the server you would like to join.
+5) Once the server is added, click the server and "Join Server".
+6) Application will load and take you into the game.
+7) To exit, please press "ESC' key.
