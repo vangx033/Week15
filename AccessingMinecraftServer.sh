7d59aeab209f9747246ff546b6165286286df48d
vangx033 <seeho.vang@hotmail.com>
2018-04-25 18:55:58 -0700

    AccessingMinecraftServer.md

--- AccessingMinecraftServer.sh
+++ AccessingMinecraftServer.sh
@@ -1,4 +1,4 @@
-Here are the steps on how to connect to the Minecraft server, please skip to step #5
+Here are the steps on how to connect to the Minecraft server, please skip to step #4.
 ---
 1) Go to https://minecraft.net/en-us and click 'Buy Minecraft' to purchase the game.
 2) Enter your address email, password and DOB to create an account
